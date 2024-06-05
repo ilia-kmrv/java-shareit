1b1e2eeb4a169f158f807546a3de94fbe5b18d82
ilia-kmrv <i.v.kmrv@yandex.ru>
2024-06-05 21:31:12 +0300

    feat: подкл. к бд, jpa для users

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,19 +1,29 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+
+import lombok.*;
+
+import javax.persistence.*;
 
 /**
  * TODO Sprint add-controllers.
  */
-@Data
+
 @Builder(toBuilder = true)
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String email;
 }
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -3,43 +3,45 @@ package ru.practicum.shareit.user.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.exception.ResourceAlreadyExistsException;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.ResourceNotFoundException;
+import ru.practicum.shareit.user.dao.UserRepository;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.dao.UserStorage;
 import ru.practicum.shareit.util.Util;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserStorage userStorage;
+    private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public User addUser(User user) {
         log.debug("Обработка запроса на создание пользователя");
-        checkEmail(user);
-        return userStorage.create(user);
+        return userRepository.save(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUser(Long id) {
         log.debug("Получение пользователя по id={}", id);
-        return userStorage.get(id)
+        return userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("Пользователь с id=%d не найден", id)));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Collection<User> getAllUsers() {
         log.debug("Обработка запроса на получение списка пользователей");
-        return userStorage.getAll().stream().collect(Collectors.toList());
+        return userRepository.findAll();
     }
 
     @Override
+    @Transactional
     public User updateUser(User user, Long id) {
         log.debug("Обработка запроса на обновление пользователя c id={}", user.getId());
         User userInDb = getUser(id);
@@ -47,23 +49,15 @@ public class UserServiceImpl implements UserService {
                 .name(Util.isBlank(user.getName()) ? userInDb.getName() : user.getName())
                 .email(user.getEmail() == null ? userInDb.getEmail() : user.getEmail())
                 .build();
-        checkEmail(userForUpdate);
-        return userStorage.update(userForUpdate);
+        return userRepository.save(userForUpdate);
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         log.debug("Обработка запроса на удаление пользователя с id={}", id);
         getUser(id);
-        userStorage.delete(id);
+        userRepository.deleteById(id);
     }
 
-    private void checkEmail(User user) {
-        boolean isNotUniqueEmail = userStorage.getAll().stream()
-                .anyMatch(u -> u.getEmail().equals(user.getEmail()) && !u.getId().equals(user.getId()));
-
-        if (isNotUniqueEmail) {
-            throw new ResourceAlreadyExistsException(String.format("Указанный Email %s уже занят", user.getEmail()));
-        }
-    }
 }
